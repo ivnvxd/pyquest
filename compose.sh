69115b5867d2b8730dd93dc191fb0e23fe010ecd
Andrey Ivanov <ivnv.xd@gmail.com>
2023-04-16 17:43:57 +0200

    add readme compose script

---
+++ compose.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+jupyter nbconvert --to markdown "01_data_structures.ipynb" --output "01_data_structures.md"
+# jupyter nbconvert --to markdown "02.ipynb" --output "02.md"
+
+output_file="README.md"
+: > "$output_file"
+
+files_to_concatenate=("intro.md"
+"table_of_contents.md"
+"01_data_structures.md" 
+"outro.md"
+)
+
+for markdown_file in "${files_to_concatenate[@]}"; do
+    cat "$markdown_file" >> "$output_file"
+    echo -e "\n" >> "$output_file"
+done
+
+rm "01_data_structures.md"
+# rm "01_data_structures.md" "02.md"
+
+echo "Successfully combined Markdown files into '$output_file'"
